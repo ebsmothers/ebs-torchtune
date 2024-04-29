4bf767e94e0fd17fa5d5c16381dc8f346b45c680
Evan Smothers <ebs@fb.com>
2024-04-29 16:46:02 -0700

    reduce bsz

--- tests/torchtune/models/test_mistral_classifier.py
+++ tests/torchtune/models/test_mistral_classifier.py
@@ -30,7 +30,7 @@ class TestMistralClassifier:
         "bsz, embed_dim, seq_len, n_classes, expected",
         [
             (2, 64, 64, 2, 22.6879),
-            (64, 128, 128, 200, 36.8238),
+            (4, 128, 128, 200, 36.8239),
             (1, 256, 256, 1, 110.2561),
         ],
     )
